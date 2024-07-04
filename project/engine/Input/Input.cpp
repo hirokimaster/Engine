f200381cbad27662b9d93b7c5c719840c17d43d8
ニヘイリュウダイ <115063428+hirokimaster@users.noreply.github.com>
2024-07-04 10:23:17 +0900

    inputの機能を最新版に更新

--- project/engine/Input/Input.cpp
+++ project/engine/Input/Input.cpp
@@ -1,12 +1,12 @@
 #include "Input.h"
 
-Input* Input::GetInstance(){
+Input* Input::GetInstance() {
 
 	static Input instance;
 	return &instance;
 }
 
-void Input::Initialize(){
+void Input::Initialize() {
 
 	HRESULT result{};
 	// InputDeviceの作成
@@ -26,10 +26,12 @@ void Input::Initialize(){
 	result = Input::GetInstance()->keyboard->SetCooperativeLevel(
 		WinApp::GetInstance()->GetHwnd(),
 		DISCL_FOREGROUND | DISCL_NONEXCLUSIVE | DISCL_NOWINKEY);
-	
+
+
+
 }
 
-void Input::Update(){
+void Input::Update() {
 
 	memcpy(Input::GetInstance()->preKeys, Input::GetInstance()->keys, 256);
 	Input::GetInstance()->keyboard->Acquire();
@@ -38,32 +40,22 @@ void Input::Update(){
 	GetJoystickState();
 }
 
-bool Input::PushKey(uint8_t keyNum){
+bool Input::PushKey(uint8_t keyNum) {
 
 	if (Input::GetInstance()->keys[keyNum] == 0x80)
 	{
 		return true;
 	}
 	return false;
-	
+
 }
 
-bool Input::PressedKey(uint32_t keyNum){
-	
+bool Input::PressedKey(uint32_t keyNum) {
+
 	if (Input::GetInstance()->keys[keyNum] == 0x80 && Input::GetInstance()->preKeys[keyNum] == 0x00)
 	{
 		return true;
 	}
-	return false;
-}
-
-bool Input::GetJoystickState(XINPUT_STATE& out) const
-{
-	DWORD dwResult = XInputGetState(0, &out);
-	if (dwResult == ERROR_SUCCESS) {
-		return true;
-	}
-
 	return false;
 }
 
@@ -77,8 +69,19 @@ bool Input::GetJoystickState()
 	return false;
 }
 
+bool Input::GetJoystickState(XINPUT_STATE& out)
+{
+	DWORD dwResult = XInputGetState(0, &out);
+	if (dwResult == ERROR_SUCCESS) {
+		return true;
+	}
+
+	return false;
+}
+
 bool Input::PressedButton(WORD button)
 {
+
 	bool flag = false;
 
 	if (Input::GetInstance()->preState_.Gamepad.wButtons & button)
@@ -93,12 +96,3 @@ bool Input::PressedButton(WORD button)
 
 	return false;
 }
-
-void Input::UpdateButtonState(ButtonState& state, bool isPressed)
-{
-	state.wasPressed = state.isPressed;
-	state.isPressed = isPressed;
-}
-
-
-
