5d5e19e6683ec0085bfe284a527742871564ee50
ニヘイリュウダイ <115063428+hirokimaster@users.noreply.github.com>
2025-05-13 03:05:12 +0900

    弾の軌跡のクラスだけ作った

---
+++ project/application/GameObject/Bullet/BulletTrajectory/BulletTrajectory.cpp
@@ -0,0 +1,15 @@
+#include "BulletTrajectory.h"
+/**
+* @file BulletTrajectory.cpp
+* @brief 弾の軌跡
+* @author 仁平 琉乃
+*/
+
+void BulletTrajectory::Initialize()
+{
+
+}
+
+void BulletTrajectory::Update()
+{
+}
--- project/application/GameObject/Bullet/BulletTrajectory/BulletTrajectory.h
+++ project/application/GameObject/Bullet/BulletTrajectory/BulletTrajectory.h
@@ -1,1 +1,26 @@
+/**
+* @file BulletTrajectory.h
+* @brief 弾の軌跡
+* @author 仁平 琉乃
+*/
+
 #pragma once
+#include "engine/3d/BaseObject/BaseInstancingObject.h"
+
+class BulletTrajectory : public BaseInstancingObject{
+public:
+
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize();
+
+	/// <summary>
+	/// 更新
+	/// </summary>
+	void Update();
+
+private:
+
+
+};
